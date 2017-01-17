9bb298d013e4d698b36752e212a53be4db06db0b
jags2-0 <jessica@boomtownroi.com>
2017-01-17 16:47:57 -0500

    I have a working walley robot!

--- robot.rb
+++ robot.rb
@@ -51,7 +51,7 @@ attr_reader :direction
     end
 
     if @direction == "east"
-      @x -= 1
+      @x += 1
     end
 
     if @direction == "west"
@@ -69,27 +69,18 @@ walley = Robot.new(0, 0, 'north')
 walley.turn('left')
 puts "I'm at direction #{walley.direction}"
 
-# walley.turn('left')
-# walley.move(1)
-# walley.move(1)
-# walley.turn('left')
-# walley.move(1)
-# walley.position
+walley.turn('left')
+puts "I'm at direction #{walley.direction}"
 
-# start is 0,0, North
-#   turn left
-#   turn left
-# position is 0,0, South
-#   move forward
-#   move forward
-# position is 0,-2 South
-#   turn left
-# postiion is 0,-2 East
-#   move forward
-# position is 1,-2 facing East
+walley.move(1)
+puts "#{walley.position}"
 
+walley.move(1)
+puts "#{walley.position}"
 
-# start at x=0 and y=0, facing north
-# could be told to move forward 1 unit, or to turn left or right by 90 degrees
-# will be asked for your coordinates after you're done
-# So move "LLFFLF" turns to face south, moves down 2 units, turns left (now facing east), and moves forward, ending at x=1 and y=-2.
+walley.turn('left')
+puts "I'm at direction #{walley.direction}"
+
+walley.move(1)
+puts "#{walley.position}"
+
